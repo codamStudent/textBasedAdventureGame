1f93b99d3fb8ce16d5f79068ce2b0a37ed314e8d
codamStudent <37171@ma-web.nl>
2023-10-03 10:37:11 +0200

    progress till nu

---
+++ interface.js
@@ -0,0 +1,49 @@
+var rs = require("readline-sync")
+
+function fillSpacesCentered(input, size) {
+    
+}
+
+
+
+
+
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("                                                                                                                                                            ")
+console.log("  _________________________                                                                                                                                 ")
+console.log(" |                         |                                                                                                                                ")
+console.log(" |                         |                                                                                                                                ")
+console.log(" |                         |                                                                                                                                ")
+console.log(" |                         |                                                                                                                                ")
+console.log(" |_________________________|______________________________________________________________________________________________________________________________  ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |                                                                                                                                                        | ")
+console.log(" |________________________________________________________________________________________________________________________________________________________| ")
+console.log("                                                                                                                                                            ")
+var answer = rs.question()
