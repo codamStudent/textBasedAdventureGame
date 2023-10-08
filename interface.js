16eaaefbf702012829ce2029a32ac5f90cc14b17
codamStudent <37171@ma-web.nl>
2023-10-08 07:41:04 +0200

    begonnen aan showscreen

--- interface.js
+++ interface.js
@@ -2,57 +2,60 @@ var rs = require("readline-sync")
 class Hud{
     static fillSpacesCentered(input, size) {
         let flipflop = false
-        if (flipflop) {
-            input = input + ' '
-        }else{
-            input = ' ' + input
-        }
+        if (flipflop) input = input + ' '
+        else input = ' ' + input
+        
         return input
     }
-}
+
 // const hud = new Hud();
+
+
+// the final part that shows the dialogue and image and gets the user input
+static showScreen(CName, dialogue, image){
+    
+    console.log("                                      ------------------------------                                                                                        ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("                                                                                                                                                            ")
+    console.log("  _________________________                                                                                                                                 ")
+    console.log(" |                         |                                                                                                                                ")
+    console.log(" |                         |                                                                                                                                ")
+    console.log(" |" + this.fillSpacesCentered(CName, 25) + "|                                                                                                                                ")
+    console.log(" |                         |                                                                                                                                ")
+    console.log(" |_________________________|______________________________________________________________________________________________________________________________  ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |                                                                                                                                                        | ")
+    console.log(" |________________________________________________________________________________________________________________________________________________________| ")
+    console.log("                                                                                                                                                            ")
+    }
+}
 console.log(Hud.fillSpacesCentered("jannes", 9))
 
-
-
-// console.log("                                      ------------------------------                                                                                        ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("                                                                                                                                                            ")
-// console.log("  _________________________                                                                                                                                 ")
-// console.log(" |                         |                                                                                                                                ")
-// console.log(" |                         |                                                                                                                                ")
-// console.log(" |                         |                                                                                                                                ")
-// console.log(" |                         |                                                                                                                                ")
-// console.log(" |_________________________|______________________________________________________________________________________________________________________________  ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |                                                                                                                                                        | ")
-// console.log(" |________________________________________________________________________________________________________________________________________________________| ")
-// console.log("                                                                                                                                                            ")
-// var answer = rs.question()
-// module.exports = Hud
+var answer = rs.question()
+module.exports = Hud
