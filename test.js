f3c56080a4ff09dfe0415206495c1395393586d9
codamStudent <37171@ma-web.nl>
2023-10-23 23:48:38 +0200

    everything i worked on today

--- test.js
+++ test.js
@@ -5,4 +5,24 @@
 // console.log(string.slice(10, 15))
 // console.log(string.slice(15, 20))
 // console.log(__dirname)
-console.log("|" + "                 " +"\\");
+// console.log("|" + "                 " +"\\");
+// const { properties } = require('./test.json');    
+// let json = require('node:json')
+
+// JSON.
+
+// console.log(properties.ReferenceToLocalSchema.ref);
+
+// "properties": {
+//     "ReferenceToLocalSchema
+
+let Max_length = 69, string = "sASDFASDasdfssadjfkladsjflkajsdfasdfkljasdfakldshfdashasd fasdf argbys fbwrsb", printString = ""
+
+function cutString(Max_length, string) {
+    for (let index = Max_length; index >= 0; index--) 
+        if (string[index] == '\n' || string[index] == ' ' ) {
+            return string.substring(0, index); break
+        }
+}
+
+console.log(cutString(Max_length, string));
