c7b6b1f99b0e965222300c772105319159d62297
codamStudent <37171@ma-web.nl>
2023-10-27 19:56:05 +0200

    why tf is test.js throwing an error

--- interface.js
+++ interface.js
@@ -1,4 +1,5 @@
 var rs = require("readline-sync")
+var temp = 0, temp2 = 0
 class Hud{
     //TODO: test all fillspaces to their function name
     static fillSpacesRight(input, size){
@@ -32,6 +33,12 @@ class Hud{
             size--
         }
         return input
+    }
+
+    static cutString(string, Max_length ) {
+        for (let index = Max_length; index >= 0; index--)
+            if (string[index] == '\n' || string[index] == ' ' )
+                return [string.substring(0, index), Max_length];
     }
 
 // const hud = new Hud();
@@ -74,6 +81,13 @@ static showScreenDialogue(CName, dialogue, image, question){
     console.log(" |                         |                                                                                                                                ")
     console.log(" |_________________________|______________________________________________________________________________________________________________________________  ")
 
+    [temp, temp2] = this.cutString(dialogue, LineLength)
+    console.log(" |"+ this.fillSpacesRight(temp, LineLength)+"| ");
+    
+    [temp, temp2] = this.cutString(dialogue.substring(temp2, dialogue.length))
+    console.log(" |"+ this.fillSpacesRight(temp, LineLength)+"| ");
+    // this.fillSpacesRight(this.cutString(dialogue, LineLength))
+    
     //this ensures good line wrapping(, hopefully, havent tested it yet)
     for (let Line = 0; Line <= 8; Line++) {
         console.log(" |" + dialogue.slice(0 + (LineLength * Line), (LineLength * (Line+1))) + "| ")
@@ -85,6 +99,15 @@ static showScreenDialogue(CName, dialogue, image, question){
     
     return rs.question(question)
     }
+
+    /*let Max_length = 69, string = "sASDFASDasdfssadjfkladsjflkajsdfasdfkljasdfakldshfdashasd fasdf argbys fbwrsb", printString = ""
+
+function cutString(Max_length, string) {
+    for (let index = Max_length; index >= 0; index--)
+        if (string[index] == '\n' || string[index] == ' ' )
+            return string.substring(0, index);
+}*/
+
     static showScreenQuestion(CName, dialogue, image, question){
         
         console.log("                                      ------------------------------                                                                                        ")
--- test.js
+++ test.js
@@ -16,13 +16,90 @@
 // "properties": {
 //     "ReferenceToLocalSchema
 
-let Max_length = 69, string = "sASDFASDasdfssadjfkladsjflkajsdfasdfkljasdfakldshfdashasd fasdf argbys fbwrsb", printString = ""
+// let Max_length = 69, string = "sASDFASDasdfssadjfkladsjflkajsdfasdfkljasdfakldshfdashasd fasdf argbys fbwrsb", printString = ""
 
-function cutString(Max_length, string) {
-    for (let index = Max_length; index >= 0; index--)
-        if (string[index] == '\n' || string[index] == ' ' )
-            return string.substring(0, index);
+// function cutString(Max_length, string) {
+//     for (let index = Max_length; index >= 0; index--)
+//         if (string[index] == '\n' || string[index] == ' ' )
+//             return string.substring(0, index);
+// }
+
+
+// console.log(cutString(Max_length, string));
+
+function returnTwo() {
+    return ["aaaaaa", 7]}
+// }
+// function printTwo(a, b) {
+//     console.log(a);
+//     console.log(b);
+// }
+
+let aaa, bbb, ccc
+
+[aaa, bbb] = returnTwo()
+console.log(bbb);
+
+// [aaa,bbb] = [3,2]
+// console.log(aaa);
+// console.log(bbb); 
+// [ccc] = returnTwo()
+// console.log(ccc);
+// printTwo([ccc] = returnTwo(),aaa)
+
+// let eee = ["aaaaaaaaaaaa", 34] + 1
+// console.log(eee[15]);
+var texttobeprinted = "", lastprintedchar = 0, LineLength = 30 
+let dialogue = "aaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbb cccccccccccccccccccccccc dddddddddddddddddd eeeeeeeeeeeeee"
+
+function fillSpacesRight(input, size){
+    size -= input.length
+
+    while (size != 0) {
+        input = input + ' '
+        size--;
+    }
+    return input
 }
 
+function cutString(string, Max_length ) {
+    for (let index = Max_length; index >= 0; index--)
+        if (string[index] == '\n' || string[index] == ' ' )
+            return [string.substring(0, index), index];
+}
 
-console.log(cutString(Max_length, string));
+function cutStringTwice(string, Start, Max_length ) {
+	// console.log(Start, Max_length);
+    for (let index = Max_length; index >= 0; index--){
+
+        if (string[Start + index] == ' '){									/*string[Start + index] == '\n' || */
+
+            return [string.substring(Start, Start + index), Start + index];
+}}}
+
+
+
+// [texttobeprinted, lastprintedchar] = cutStringTwice(dialogue, 40, LineLength)
+// console.log(texttobeprinted, lastprintedchar);
+   
+
+
+
+// [texttobeprinted, lastPrintedhar] = cutStringTwice(dialogue, lastprintedchar, LineLength)
+    // console.log(" |"+ fillSpacesRight(texttobeprinted, LineLength)+"| ");
+
+    // // [texttobeprinted, lastprintedchar] = cutString(dialogue.substring(lastprintedchar, dialogue.length))
+    // console.log(" |"+ fillSpacesRight(texttobeprinted, LineLength)+"| ");
+    // // fillSpacesRight(cutString(dialogue, LineLength))
+    
+    //this ensures good line wrapping(, hopefully, havent tested it yet)
+    for (let Line = 0; Line <= 8; Line++) {
+
+
+		[texttobeprinted, lastprintedchar] = cutStringTwice(dialogue, lastprintedchar, LineLength);
+		// console.log(texttobeprinted+'a');
+		console.log(lastprintedchar++);
+		console.log(" |"+ fillSpacesRight(texttobeprinted, LineLength)+"| ");
+
+        // console.log(" |" + dialogue.slice(0 + (LineLength * Line), (LineLength * (Line+1))) + "| ")
+    }
